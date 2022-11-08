c77aa9cf57729982a61bfce44943d8e10747ad96
1Alisa1 <Alisa.dragun@mail.ru>
2022-11-08 18:11:35 +0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+import AboutPage from './pages/aboutPage';
+import HomePage from './pages/homePage';
+import BlogPage from './pages/blogPage';
+import NotFoundPage from './pages/notFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <header>
+        <Link to='/'>Home</Link>
+        <Link to='/blog'>Blog</Link>
+        <Link to='/about'>About</Link>
+      </header>
+      <Routes>
+        <Route path='/' element={<HomePage />} />;
+        <Route path='/about' element={<AboutPage />} />;
+        <Route path='/blog' element={<BlogPage />} />;
+        <Route path='*' element={<NotFoundPage />} />;
+      </Routes>
+    </>
   );
 }
 
